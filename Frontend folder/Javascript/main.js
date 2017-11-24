babdd626af32aa45d6709c312a44f01a349d7557
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2017-11-25 01:39:39 +0700

    Make a mobile friendly

--- Frontend folder/Javascript/main.js
+++ Frontend folder/Javascript/main.js
@@ -1,4 +1,10 @@
 $(function(){
-    $('.side-navbar').css('display', 'none').show(1500)
-    $('.frame-navbar').css('display', 'none').show(665)
+    $('.side-navbar').css('display', 'none').show(1500);
+    $('.frame-navbar').css('display', 'none').show(665);
+    $('#option-icon').css('display', 'none');
+    if((screen.width <= 1024)){
+        $('.side-navbar').css('display', 'none');
+        $('#option-icon').show(100)
+    }
+    
 });
