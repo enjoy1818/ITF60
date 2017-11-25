f5c2cdbc4e74e4159fcb00333543b8d378a93605
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2017-11-25 12:57:39 +0700

    add a user panel

--- Frontend folder/Javascript/main.js
+++ Frontend folder/Javascript/main.js
@@ -1,11 +1,11 @@
 $(function(){
+    $('.group-list').css('display', 'none').show(1600)
     $('.side-navbar').css('display', 'none').show(1500);
     $('.frame-navbar').css('display', 'none').show(665);
-    $('.user-image').css('display', 'none').show(1500);
+    $('.user-image').css('display', 'none').delay(1500).show(100);
     $('.option').css('display', 'none');
     if((screen.width <= 1024)){
         $('.side-navbar').hide();
         $('.option').show(100);
     }
-    
 });
