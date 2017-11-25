fce47cb231aec43e53789e561f494a10710704f0
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2017-11-25 16:33:24 +0700

    sdasdsad

--- Frontend folder/Javascript/main.js
+++ Frontend folder/Javascript/main.js
@@ -3,14 +3,4 @@ $(function() {
     $('.side-navbar').css({display: 'none'}).show(1500);
     $('.frame-navbar').css({display: 'none'}).show(665);
     $('.user-image').css({display: 'none'}).delay(1500).show(100);
-    $('.option').css({display: 'none'});
-    if (screen.width <= 1024) {
-        $('.side-navbar').css({display: 'none'})
-        $('.option').css({display: 'block'});
-    }
-    else {
-    
-        alert('More than 1024');
-    }
-    
 });
