2bb96fe6f9b073fa415d0d2ee75a70c994690037
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2017-11-25 21:55:13 +0700

    asdsad

--- Frontend folder/Javascript/main.js
+++ Frontend folder/Javascript/main.js
@@ -1,9 +1,3 @@
 $(function() {
-    $('.side-nav-toggle').click(function() {
-        $('.side-navbar').show(100);
-    });
-    $('.side-nav-close').click(function() {
-        $('.side-navbar').hide();
-        location.reload(true);
-    });
+
 });
