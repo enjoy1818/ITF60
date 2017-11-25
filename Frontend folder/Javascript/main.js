0d83336dbf24807cf2d296ed5ede2dc9e3cf95c6
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2017-11-25 15:16:30 +0700

    Big update

--- Frontend folder/Javascript/main.js
+++ Frontend folder/Javascript/main.js
@@ -1,11 +1,7 @@
 $(function(){
-    $('.group-list').css('display', 'none').show(1600)
+    $('.group-list').css('display', 'none').show(1600);
     $('.side-navbar').css('display', 'none').show(1500);
     $('.frame-navbar').css('display', 'none').show(665);
     $('.user-image').css('display', 'none').delay(1500).show(100);
     $('.option').css('display', 'none');
-    if((screen.width <= 1024)){
-        $('.side-navbar').hide();
-        $('.option').show(100);
-    }
 });
