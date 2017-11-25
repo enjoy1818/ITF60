ffe7cff78831b2b592dfc6ff3eaf52d9af41add5
enjoy1818 <32461841+enjoy1818@users.noreply.github.com>
2017-11-25 12:35:12 +0700

    add user image

--- Frontend folder/Javascript/main.js
+++ Frontend folder/Javascript/main.js
@@ -1,10 +1,11 @@
 $(function(){
     $('.side-navbar').css('display', 'none').show(1500);
     $('.frame-navbar').css('display', 'none').show(665);
-    $('#option-icon').css('display', 'none');
+    $('.user-image').css('display', 'none').show(1500);
+    $('.option').css('display', 'none');
     if((screen.width <= 1024)){
-        $('.side-navbar').css('display', 'none');
-        $('#option-icon').show(100)
+        $('.side-navbar').hide();
+        $('.option').show(100);
     }
     
 });
